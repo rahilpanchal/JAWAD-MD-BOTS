a66bfe8a79b671432ba2deff015e04b0ee227242
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-03-25 10:52:53 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ const ping = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363354023106228@newsletter',
+          newsletterJid: '120363398040175935@newsletter',
           newsletterName: "JawadTechX",
           serverMessageId: 143
         }
